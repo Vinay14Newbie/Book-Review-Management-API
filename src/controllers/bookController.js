6467b221b26862f1dd005ab5f2896fbd66af5b8b
Vinay14Newbie <wjust1984@gmail.com>
2025-06-22 18:09:26 +0530

    Added get books api

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,4 +1,5 @@
 import Book from '../schema/book.js';
+import Review from '../schema/review.js';
 
 export const addBook = async (req, res) => {
   try {
@@ -14,7 +15,47 @@ export const addBook = async (req, res) => {
       message: 'Book added successfully'
     });
   } catch (error) {
-    console.log('Error found adding new book');
+    console.log('Error found while adding new book');
+    return res.status(500).json({
+      status: false,
+      message: 'Internal error'
+    });
+  }
+};
+
+export const getBooks = async (req, res) => {
+  try {
+    const { genre, author, page = 1, limit = 10 } = req.query;
+    const query = {};
+    if (genre) query.genre = genre;
+    if (author) query.author = author;
+
+    const books = await Book.find(query)
+      .skip((page - 1) * limit)
+      .limit(Number(limit));
+
+    const data = await Promise.all(
+      books.map(async (book) => {
+        const reviews = await Review.find({ bookId: book._id });
+
+        const avgRating = reviews.length
+          ? reviews.reduce((sum, r) => sum + r.rating, 0) / reviews.length
+          : 0;
+
+        return {
+          ...book.toObject(),
+          averageRating: parseFloat(avgRating.toFixed(1))
+        };
+      })
+    );
+
+    res.status(200).json({
+      status: true,
+      data: data,
+      message: 'Book info fetched successfully'
+    });
+  } catch (err) {
+    console.log('Error found while adding fetching books');
     return res.status(500).json({
       status: false,
       message: 'Internal error'
--- src/routers/bookRoutes.js
+++ src/routers/bookRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { addBook } from '../controllers/bookController.js';
+import { addBook, getBooks } from '../controllers/bookController.js';
 import { validate } from '../validators/zodValidator.js';
 import { zodBookSchema } from '../validators/zodBookSchema.js';
 
 const router = express.Router();
 
 router.post('/', validate(zodBookSchema), addBook);
+
+router.get('/', getBooks);
 
 export default router;
